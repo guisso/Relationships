41650daa9842eca4f42516fc5170b8146cfd1ae4
Guisso <luis.guisso@ifnmg.edu.br>
2025-08-18 12:16:11 -0300

    Unit tests for Nascimento

--- RelationshipsOneToOne.Tests/UnitTest1.cs
+++ RelationshipsOneToOne.Tests/UnitTest1.cs
@@ -64,6 +64,45 @@ namespace RelationshipsOneToOne.Tests
             Assert.Equal(cpf, cidadao.Cpf);
         }
 
+        //
+        // Nascimento
+        //
+
+        [Fact]
+        public void NascimentoNull_ThrowsArgumentNullException()
+        {
+            Cidadao cidadao = new Cidadao();
+            Assert.Throws<ArgumentNullException>(() => cidadao.Nascimento = null);
+        }
+
+        [Fact]
+        public void NascimentoLessThan18Years_ThrowArgumentException()
+        {
+            Cidadao cidadao = new Cidadao();
+            cidadao.Nascimento = DateOnly.FromDateTime(DateTime.Now).AddYears(-18).AddDays(1);
+        }
+
+        [Fact]
+        public void Nascimento18Years_SetsNascimento()
+        {
+            DateOnly idade18 = DateOnly.FromDateTime(DateTime.Now).AddYears(-18);
+
+            Cidadao cidadao = new Cidadao();
+            cidadao.Nascimento = DateOnly.FromDateTime(DateTime.Now).AddYears(-18);
+
+            Assert.Equal(idade18, cidadao.Nascimento);
+        }
+
+        [Fact]
+        public void Nascimento18Years1Day_SetsNascimento()
+        {
+            DateOnly idade18Mais = DateOnly.FromDateTime(DateTime.Now).AddYears(-18).AddDays(-1);
+
+            Cidadao cidadao = new Cidadao();
+            cidadao.Nascimento = DateOnly.FromDateTime(DateTime.Now).AddYears(-18).AddDays(-1);
+
+            Assert.Equal(idade18Mais, cidadao.Nascimento);
+        }
 
     }
 }
