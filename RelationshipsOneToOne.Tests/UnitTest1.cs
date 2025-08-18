cc39c5f4c33135beb9aed03f4a4f8bf88eacb83a
Guisso <luis.guisso@ifnmg.edu.br>
2025-08-17 22:43:32 -0300

    Test for 'nome' lengths

--- RelationshipsOneToOne.Tests/UnitTest1.cs
+++ RelationshipsOneToOne.Tests/UnitTest1.cs
@@ -9,5 +9,24 @@ namespace RelationshipsOneToOne.Tests
 
             Assert.Throws<ArgumentNullException>(() => cidadao.Nome = null);
         }
+
+        [Fact]
+        public void NomeMoreThan45Characteres_ThrowsArgumentException()
+        {
+            Cidadao cidadao = new Cidadao();
+
+            Assert.Throws<ArgumentException>(() => cidadao.Nome = "123456789_123456789_123456789_123456789_123456");
+        }
+
+        [Fact]
+        public void NomeLessThan45Characteres_SetsNome()
+        {
+            String nome = "123456789_123456789_123456789_123456789_12345";
+
+            Cidadao cidadao = new Cidadao();
+            cidadao.Nome = nome;
+
+            Assert.Equal(nome, cidadao.Nome);
+        }
     }
 }
