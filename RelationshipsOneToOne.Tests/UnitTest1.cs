d554b091db6c288b84a5c1a76c5faac16c8d4381
Guisso <luis.guisso@ifnmg.edu.br>
2025-08-18 10:19:33 -0300

    Unit tests for CPF

--- RelationshipsOneToOne.Tests/UnitTest1.cs
+++ RelationshipsOneToOne.Tests/UnitTest1.cs
@@ -2,6 +2,10 @@ namespace RelationshipsOneToOne.Tests
 {
     public class UnitTest1
     {
+        //
+        // Nome
+        //
+
         [Fact]
         public void NomeNull_ThrowsArgumentNullException()
         {
@@ -28,5 +32,38 @@ namespace RelationshipsOneToOne.Tests
 
             Assert.Equal(nome, cidadao.Nome);
         }
+
+        //
+        // CPF
+        //
+
+        [Fact]
+        public void CpfNull_ThrowsArgumentNullException()
+        {
+            Cidadao cidadao = new Cidadao();
+
+            Assert.Throws<ArgumentNullException>(() => cidadao.Cpf = null);
+        }
+
+        [Fact]
+        public void CpfMoreThan11Characteres_ThrowsArgumentException()
+        {
+            Cidadao cidadao = new Cidadao();
+
+            Assert.Throws<ArgumentException>(() => cidadao.Cpf = 100000000000);
+        }
+
+        [Fact]
+        public void CpfUntil11Characteres_SetsCpf()
+        {
+            UInt64 cpf = 123456789_01;
+
+            Cidadao cidadao = new Cidadao();
+            cidadao.Cpf = cpf;
+
+            Assert.Equal(cpf, cidadao.Cpf);
+        }
+
+
     }
 }
