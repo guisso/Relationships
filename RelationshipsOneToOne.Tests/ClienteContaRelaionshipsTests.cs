0c16df555c6ef6c57a4a0339f5ce455ee012518e
Guisso <luis.guisso@ifnmg.edu.br>
2025-08-19 16:06:07 -0300

    Cliente/ContaBancaria relationship tests

---
+++ RelationshipsOneToOne.Tests/ClienteContaRelaionshipsTests.cs
@@ -0,0 +1,61 @@
+﻿namespace RelationshipsOneToOne.Tests
+{
+    public class ClienteContaRelaionshipsTests
+    {
+        [Fact]
+        public void ClienteContaBancariaNullClienteOnContaBancaria_ThrowsArgumentNullException()
+        {
+            ContaBancaria conta = new ContaBancaria();
+            Assert.Throws<ArgumentNullException>(() => conta.Cliente = null);
+        }
+
+        [Fact]
+        public void ClienteContaBancariaNullContaBancariaOnCliente_ThrowsArgumentNullException()
+        {
+            ContaBancaria conta = new ContaBancaria
+            {
+                Abertura = DateTime.Now
+            };
+            Assert.Throws<ArgumentNullException>(() => new Cliente { ContaBancaria = null });
+        }
+
+        [Fact]
+        public void ClienteContaBancariaManualBidirectional_AssignsSuccessfully()
+        {
+            ContaBancaria conta = new ContaBancaria
+            {
+                Abertura = DateTime.Now
+            };
+
+            Cliente cliente = new Cliente
+            {
+                Nome = "Ana Zaira",
+                Cpf = 999_999_999_99,
+                Telefone = 11987654321
+            };
+
+            cliente.ContaBancaria = conta;
+
+            Assert.Equal(cliente, conta.Cliente);
+        }
+
+        [Fact]
+        public void ClienteContaBancariaAutomaticBidirectional_AssignsSuccessfully()
+        {
+            ContaBancaria conta = new ContaBancaria
+            {
+                Abertura = DateTime.Now
+            };
+
+            Cliente cliente = new Cliente
+            {
+                Nome = "Ana Zaira",
+                Cpf = 999_999_999_99,
+                Telefone = 11987654321,
+                ContaBancaria = conta
+            };
+
+            Assert.Equal(cliente, conta.Cliente);
+        }
+    }
+}
